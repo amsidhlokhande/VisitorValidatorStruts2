469397abfccade53021b40af7fed964c27f5ca95
amsidhlokhande <amsidhlokhande@gmail.com>
2020-04-19 22:51:12 +0530

    Initial Commit

---
+++ src/cust/Customer.java
@@ -0,0 +1,41 @@
+package cust;
+
+import adrs.Address;
+
+public class Customer
+{
+	private String firstName;
+	private String lastName;
+	private Address address;
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+
+	public Address getAddress()
+	{
+		return address;
+	}
+
+	public void setAddress(Address address)
+	{
+		this.address = address;
+	}
+
+}
